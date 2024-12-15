b8c888c5649dd9d1f71e095184aa189651414693
Victor Matheus <victormatheus507@gmail.com>
2024-12-15 10:29:31 -0300

    finalizano pagina inicial

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,19 @@
+import { Button } from '@/components/button';
 import { Steps } from '@/components/steps';
 import { Welcome } from '@/components/welcome';
 import { View, Text } from 'react-native';
+import { IconPlus } from '@tabler/icons-react-native';
+import { router } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
-      < Steps />
+      <Steps />
+      <Button onPress={() => router.navigate('/home')}>
+        <Button.Title>Começar</Button.Title>
+        <Button.Icon icon={IconPlus} />
+      </Button>
     </View>
   );
 }
